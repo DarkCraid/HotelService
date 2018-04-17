1ff4fd0af890d48e8d48c0649a5288e415246145
daniel <2015030282@upsin.edu.mx>
2018-04-17 15:00:07 -0600

    full, falta bloquear los botones principales

--- app/src/main/java/com/hotelservice/hotelservice/MainActivity.java
+++ app/src/main/java/com/hotelservice/hotelservice/MainActivity.java
@@ -20,7 +20,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private dbConexion con = new dbConexion();
-    private EditText txtNH,txtruta,txtIP;
+    private EditText txtNH,txtruta,txtIP,txtusuario,txtpssw1,txtpssw2;
     private generales gen = new generales();
     private Spinner lst;
     private TextView lbTitle;
@@ -32,10 +32,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        lbTitle =   (TextView)findViewById(R.id.lbTitle);
-        txtNH   =   (EditText)findViewById(R.id.txtNH);
-        txtruta =   (EditText)findViewById(R.id.txtruta);
-        txtIP   =   (EditText)findViewById(R.id.txtIP);
+        lbTitle     =   (TextView)findViewById(R.id.lbTitle);
+
+        txtNH       =   (EditText)findViewById(R.id.txtNH);
+        txtruta     =   (EditText)findViewById(R.id.txtruta);
+        txtIP       =   (EditText)findViewById(R.id.txtIP);
+        txtusuario  =   (EditText)findViewById(R.id.txtusuario);
+        txtpssw1    =   (EditText)findViewById(R.id.txtpssw1);
+        txtpssw2    =   (EditText)findViewById(R.id.txtpssw2);
+
         lst     =   (Spinner)findViewById(R.id.lst);
         btnsave =   (Button)findViewById(R.id.btnsave);
 
@@ -56,15 +61,21 @@ public class MainActivity extends AppCompatActivity {
                     case "English":
                         lbTitle.setText("INITIAL SETUP");
                         txtruta.setHint("Web url*");
-                        txtNH.setHint("Room number*");
+                        txtNH.setHint("Room*");
                         txtIP.setHint("Server IP*");
+                        txtusuario.setHint("Master user*");
+                        txtpssw1.setHint("Password (Pw)*");
+                        txtpssw2.setHint("Confirm (Pw)*");
                         btnsave.setText("SAVE");
                         break;
                     default:
                         lbTitle.setText("CONFIGURACIÓN INICIAL");
                         txtruta.setHint("Ruta de la página*");
-                        txtNH.setHint("Número de habitación*");
+                        txtNH.setHint("Habitación*");
                         txtIP.setHint("IP srvidor*");
+                        txtusuario.setHint("Usuario maestro*");
+                        txtpssw1.setHint("Contraseña (Pw)*");
+                        txtpssw2.setHint("Confirmar (Pw)*");
                         btnsave.setText("GUARDAR");
                         break;
                 }
@@ -82,14 +93,24 @@ public class MainActivity extends AppCompatActivity {
 
         switch (lst.getSelectedItem().toString()){
             case "English":
-                if(txtNH.getText().toString().isEmpty())    errors.add("Type room number.");
-                if(txtruta.getText().toString().isEmpty())  errors.add("Type web url.");
-                if(txtIP.getText().toString().isEmpty())    errors.add("Type server IP.");
+                if(txtNH.getText().toString().isEmpty())        errors.add("Type room number.");
+                if(txtruta.getText().toString().isEmpty())      errors.add("Type web url.");
+                if(txtIP.getText().toString().isEmpty())        errors.add("Type server IP.");
+                if(txtusuario.getText().toString().isEmpty())   errors.add("Type a master user.");
+                if(txtpssw1.getText().toString().isEmpty())     errors.add("Type a user password.");
+                else if(txtpssw2.getText().toString().isEmpty())     errors.add("Comfirm the user password.");
+                else if(! txtpssw2.getText().toString().equals(txtpssw1.getText().toString()))
+                    errors.add("Passwords do not match.");
                 break;
             default:
-                if(txtNH.getText().toString().isEmpty())    errors.add("Ingrese el número de la habitación.");
-                if(txtruta.getText().toString().isEmpty())  errors.add("Ingrese la ruta de la web.");
-                if(txtIP.getText().toString().isEmpty())    errors.add("Ingrese la ip del servidor.");
+                if(txtNH.getText().toString().isEmpty())        errors.add("Ingrese el número de la habitación.");
+                if(txtruta.getText().toString().isEmpty())      errors.add("Ingrese la ruta de la web.");
+                if(txtIP.getText().toString().isEmpty())        errors.add("Ingrese la ip del servidor.");
+                if(txtusuario.getText().toString().isEmpty())   errors.add("Ingrese un usuario maestro.");
+                if(txtpssw1.getText().toString().isEmpty())     errors.add("Ingrese la contraseña para el usuario.");
+                else if(txtpssw2.getText().toString().isEmpty())     errors.add("confirme la contraseña.");
+                else if(! txtpssw2.getText().toString().equals(txtpssw1.getText().toString()))
+                    errors.add("Las contraseñas no coinciden.");
                 break;
         }
 
@@ -99,6 +120,8 @@ public class MainActivity extends AppCompatActivity {
             reg.put("numeroh",txtNH.getText().toString());
             reg.put("ruta",txtruta.getText().toString());
             reg.put("ipserver",txtIP.getText().toString());
+            reg.put("user",txtusuario.getText().toString());
+            reg.put("pssw",txtpssw1.getText().toString());
             if(con.InsertTable("tabletdata",reg)==-1){
                 String cont = "";
                 switch (lst.getSelectedItem().toString()){
